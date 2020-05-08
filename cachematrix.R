9973c67ae28a8cde6043d23b5dc012ba48181853
Swara-23 <65011272+Swara-23@users.noreply.github.com>
2020-05-08 17:50:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,51 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+##This function makeCacheMatrix gets a matrix as an input, set the value of the matrix,
+#get the value of the matrix, set the inverse Matrix and get the inverse Matrix. The matrix object
+#can cache its own object. 
 
+#<<- operator is used to assign a value to an object in an environment that is different 
+#from the current environment 
+
+#take the matrix as an input
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  #set the value of the Matrix
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x         #get the value of the Matrix
+  setInverse <- function(inverse) j <<- inverse   #set the value of the invertible matrix
+  getInverse <- function() j         #get the value of the invertible matrix
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## The function cacheSolve takes the output of the previous matrix makeCacheMatrix(matrix) as an 
+# input and checks inverse matrix from makeCacheMatrix(matrix) has any value in it or not.
+# In case inverse matrix from makeCacheMatrix((matrix) is empty, it gets the original matrix data from 
+# and set the invertible  matrix by using the solve function.
+# In case inverse matrix from makeCacheMatrix((matrix) has some value in it (always works
+#after running the code 1st time), it returns a message  "Getting Cached Data" 
+#and the cached object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  #if inverse matrix is not NULL
+  if(!is.null(j)){
+    message("getting cached data")    #Type message: Getting Cached Data
+    return(j)        #return the invertible matrix
+  }
+  
+  #if value of the invertible matrix is NULL then  
+  mat <- x$get()       #get the original Matrix Data 
+  j <- solve(mat,...)  #use solve function to inverse the matrix
+  x$setInverse(j)      #set the invertible matrix 
+  j                    #return the invertible matrix
+  
+  ## Return a matrix that is the inverse of 'x'
 }
